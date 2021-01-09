dfd3581703f06f86ff29f03ec64d1ebe5aa90d4d
haileyHi <yuhiro@naver.com>
2021-01-09 21:58:24 +0900

    my page 세부 액티비티 틀

--- Trolly/app/src/main/java/com/jcozy/trolly/mypage/MypageActivity.kt
+++ Trolly/app/src/main/java/com/jcozy/trolly/mypage/MypageActivity.kt
@@ -1,12 +1,33 @@
 package com.jcozy.trolly.mypage
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.jcozy.trolly.R
 
-class MypageActivity : AppCompatActivity() {
+class MypageActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mypage)
+
+
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.view_mypage_level -> {
+                val intent = Intent(applicationContext, MypageLevelActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.view_mypage_rank -> {
+                val intent = Intent(applicationContext, MypageRankingActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.view_mypage_history -> {
+                val intent = Intent(applicationContext, HistoryActivity::class.java )
+                startActivity(intent)
+            }
+        }
     }
 }
---
+++ Trolly/app/src/main/java/com/jcozy/trolly/mypage/MypageRankingActivity.kt
@@ -0,0 +1,13 @@
+package com.jcozy.trolly.mypage
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jcozy.trolly.R
+
+class MypageRankingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ranking)
+
+    }
+}
